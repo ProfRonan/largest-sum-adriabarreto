575eef8c336d40975b8b510161c11dfc9e17083a
Adria Barreto <adriabarreto12@gmail.com>
2023-05-26 08:34:42 -0300

    Exercício Concluído

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ def largest_sum(numbers: list[int]) -> tuple[int, int]:
     """Encontra e retorna os dois números que somados dão o maior valor."""
     primeiro = 0  # o primeiro número da soma
     segundo = 0  # o segundo número da soma
+    numbers.sort()
+    if len(numbers) < 2:
+        return None
+    if len(numbers) > 2:
+        primeiro = numbers[-2]
+        segundo = numbers[-1]
     return primeiro, segundo
